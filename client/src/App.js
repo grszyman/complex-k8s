3ca5591860500fe050748c7746370149456b4418
Grzegorz Szymański <grszyman@gmail.com>
2022-11-13 11:53:50 +0100

    small change in the app v7

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>FibCalculator v6 - feature branch</p>
+          <p>FibCalculator v7 - feature branch</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
