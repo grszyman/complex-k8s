1eb1e93d95fbbcaa469c5e297611109f5014e5f6
Grzegorz Szymański <grszyman@gmail.com>
2022-11-13 21:22:20 +0100

    Final version 1.0.0

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>FibCalculator v11 - feature branch</p>
+          <p>FibCalculator v1.0.0</p>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
